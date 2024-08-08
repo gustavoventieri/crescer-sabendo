14f74fc490f35704ed4bcdb691f8c39b9f3c05bc
gustavoventieri <ventierigustavo@gmail.com>
2024-08-08 03:29:14 -0300

    feat: back button added

--- resources/views/user/signUp.blade.php
+++ resources/views/user/signUp.blade.php
@@ -16,6 +16,15 @@
 <body>
     <div class="relative min-h-screen flex items-center justify-center bg-cover bg-center">
         <div class="bg-white w-[450px] mx-5 px-8 py-10 rounded-3xl shadow-md border border-gray-300 relative z-10">
+            <a href="/">
+                <button class="w-20 -translate-y-4 -translate-x-3 flex items-center py-1.5 px-6 space-x-2 hover:bg-gray-200 hover:rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-log-out" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M11 5l-7 7l7 7" />
+                        <path d="M19 12h-14" />
+                    </svg>
+                </button>
+            </a>
             <div class="flex justify-center mb-6">
                 <img src="{{ asset('images/logo.png') }}" alt="Logo Crescer Sabendo" class="h-16">
             </div>
